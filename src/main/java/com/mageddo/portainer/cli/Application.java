dbe567093f0b7f2a560e1f440c09ae3b3292efab
Elvis de Freitas <edigitalb@gmail.com>
2019-07-28 18:45:05 -0300

    referring substratevm commons, fixing some reflection references

--- src/main/java/com/mageddo/portainer/cli/Application.java
+++ src/main/java/com/mageddo/portainer/cli/Application.java
@@ -1,14 +1,14 @@
 package com.mageddo.portainer.cli;
 
 import com.beust.jcommander.ParameterException;
+import com.mageddo.common.net.ssl.TrustManagerUtils;
 import com.mageddo.portainer.cli.command.PortainerCommand;
-import com.mageddo.portainer.client.utils.EnvUtils;
 
 public class Application {
 
 	public static void main(String[] args) {
 		try {
-			EnvUtils.setupEnv();
+			TrustManagerUtils.installTrustStore();
 			PortainerCommand.parseAndRun(args);
 		} catch (ParameterException e){
 			System.err.println(e.getMessage());
--- src/main/java/com/mageddo/portainer/cli/config/ReflectionClasses.java
+++ src/main/java/com/mageddo/portainer/cli/config/ReflectionClasses.java
@@ -1,73 +1,29 @@
 package com.mageddo.portainer.cli.config;
 
+import com.fasterxml.jackson.databind.ext.Java7SupportImpl;
+import com.mageddo.common.graalvm.SubstrateVM;
+import com.mageddo.portainer.cli.command.PortainerStackDeployCommand;
+import com.mageddo.portainer.cli.command.PortainerStackRunCommand;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.sql.Statement;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 @AutomaticFeature
 class ReflectionClasses implements Feature {
 
 	@Override
+	public void duringSetup(DuringSetupAccess access) {
+		System.loadLibrary("sunec");
+	}
+
+	@Override
 	public void beforeAnalysis(BeforeAnalysisAccess access) {
-		setupClasses();
-	}
-
-	/**
-	 * All classes defined here will have reflection support and be registered as spring beans
-	 */
-	static Class<?>[] getBeans(){
-		return new Class[]{
-		};
-	}
-
-	/**
-	 * All classes defined here will have reflection support
-	 */
-	static Class<?>[] getClasses(){
-		return new Class[]{
-			Statement[].class
-		};
-	}
-
-	static void setupClasses() {
-		try {
-			System.out.println("> Loading classes for future reflection support");
-			for (final Class<?> clazz : getBeans()) {
-				process(clazz);
-			}
-			for (final Class<?> clazz : getClasses()) {
-				process(clazz);
-			}
-		} catch (Error e){
-			if(!e.getMessage().contains("The class ImageSingletons can only be used when building native images")){
-				throw e;
-			}
-		}
-	}
-
-	/**
-	 * Register all constructors and methods on graalvm to reflection support at runtime
-	 */
-	private static void process(Class<?> clazz) {
-		try {
-			System.out.println("> Declaring class: " + clazz.getCanonicalName());
-			RuntimeReflection.register(clazz);
-			for (final Method method : clazz.getMethods()) {
-				System.out.println("\t> method: " + method.getName() + "(" + method.getParameterCount() + ")");
-				RuntimeReflection.register(method);
-			}
-			for (final Constructor<?> constructor : clazz.getDeclaredConstructors()) {
-				System.out.println("\t> constructor: " + constructor.getName() + "(" + Arrays.toString(constructor.getParameterTypes()) + ")");
-				RuntimeReflection.register(constructor);
-			}
-		} catch (Exception e){
-			e.printStackTrace();
-		}
+		SubstrateVM.process(ArrayList.class, Java7SupportImpl.class);
+		SubstrateVM.process(
+			true, true, true,
+			PortainerStackDeployCommand.class, PortainerStackRunCommand.class
+		);
 	}
 }
 
